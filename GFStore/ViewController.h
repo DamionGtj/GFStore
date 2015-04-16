2353b1cad0c2b2ceae556af7663206a36a32f24e
guotianji <guotianji@guotianjideiMac.local>
2015-04-16 16:40:10 +0800

    Initial Commit

---
+++ GFStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GFStore
+//
+//  Created by guotianji on 15/4/16.
+//  Copyright (c) 2015年 gtj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GFStore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GFStore
+//
+//  Created by guotianji on 15/4/16.
+//  Copyright (c) 2015年 gtj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
